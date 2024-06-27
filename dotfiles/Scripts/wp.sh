f2ded893bce24592daeee3970c66ffa06ad43a75
Alex230805 <misuralealessandro@gmail.com>
2024-06-27 08:32:22 +0200

    new version of files

--- dotfiles/Scripts/wp.sh
+++ dotfiles/Scripts/wp.sh
@@ -1,16 +1,32 @@
 #!bin/bash
 
 TOTAL_IMG=6
+TOTAL_IMG_N=4
 IMG_DIR='/home/am/Images/wp'
-TIME=420
+IMG_DIR_NIGHT='/home/am/Images/wpn'
+TIME=460
+
 
 random_number=$(shuf -i 1-$TOTAL_IMG -n 1)
+cache=$random_number
 
-CMD_STRING=$IMG_DIR'/'$random_number'.jpg'
+
+random () {
+    random_number=$(shuf -i 1-$1 -n 1)
+}
 
 
 while true
-do 
-    swaybg -m fill -i $CMD_STRING 
+do
+    time=$(date +"%H")
+    if [ "$time" -gt "22" ]
+    then
+	random $TOTAL_IMG_N
+	swaybg -m fill -i $IMG_DIR_NIGHT'/'$random_number'.jpg'
+    else
+	random $TOTAL_IMG
+	swaybg -m fill -i $IMG_DIR'/'$random_number'.jpg'
+    fi
     sleep $TIME
 done
+
