a03e002614d501c9b57c62481fc211377ebd77f4
Alex230805 <misuralealessandro@gmail.com>
2024-07-30 11:40:16 +0200

    update

--- dotfiles/Scripts/wp.sh
+++ dotfiles/Scripts/wp.sh
@@ -4,29 +4,27 @@ TOTAL_IMG=6
 TOTAL_IMG_N=4
 IMG_DIR='/home/am/Images/wp'
 IMG_DIR_NIGHT='/home/am/Images/wpn'
-TIME=460
-
-
-random_number=$(shuf -i 1-$TOTAL_IMG -n 1)
-cache=$random_number
-
+random_number=4
 
 random () {
-    random_number=$(shuf -i 1-$1 -n 1)
+	random_number=$((1 + $RANDOM % $1))
+}
+
+getWallpaper () {
+	time=$(date +"%H")
+	if [ "$time" -gt "22" ]  || [ "$time" -le "7" ]
+	then
+		random $TOTAL_IMG_N
+		swaybg -m fill -i $IMG_DIR_NIGHT'/'$random_number'.jpg'
+	else
+		random $TOTAL_IMG
+		swaybg -m fill -i $IMG_DIR'/'$random_number'.jpg'
+	fi
+	sleep 3
 }
 
 
-while true
+while :
 do
-    time=$(date +"%H")
-    if [ "$time" -gt "22" ]
-    then
-	random $TOTAL_IMG_N
-	swaybg -m fill -i $IMG_DIR_NIGHT'/'$random_number'.jpg'
-    else
-	random $TOTAL_IMG
-	swaybg -m fill -i $IMG_DIR'/'$random_number'.jpg'
-    fi
-    sleep $TIME
+	getWallpaper
 done
-
